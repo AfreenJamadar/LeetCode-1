fca35ba359db68234e17bb30d6add46a3e574089
Afreen Jamadar <afreenrjamadar@gmail.com>
2026-09-24 22:25:22 +0530

    Time: 1 ms (99.94%) | Memory: 42.8 MB (98.05%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] stack = new char[s.length()];
+        int top = -1;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack[++top] = ')';
+            } else if (c == '{') {
+                stack[++top] = '}';
+            } else if (c == '[') {
+                stack[++top] = ']';
+            } else {
+                if (top == -1 || stack[top--] != c) {
+                    return false;
+                }
+            }
+        }
+
+        return top == -1;
+    }
+}
